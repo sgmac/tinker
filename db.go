f5458fc68b2a7264d9f4b7bad3b5474199730b10
Sergio Galvan <sergalma@gmail.com>
2015-08-19 20:19:06 +0200

    Move topics own file

--- db.go
+++ db.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"path/filepath"
 
@@ -11,6 +10,7 @@ import (
 var db *bolt.DB
 
 const markDone = "\u2713"
+const check = "\u09E9"
 
 func init() {
 	var err error
@@ -19,34 +19,4 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// A new topic is mapped to a bucket.
-func addNewTopic(topic string) {
-	db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte(topic))
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-}
-
-func listTopics() {
-	db.View(func(tx *bolt.Tx) error {
-		return tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
-			fmt.Println(markDone, string(name))
-			return nil
-		})
-	})
-}
-
-func deleteTopic(topic string) {
-	db.Update(func(tx *bolt.Tx) error {
-		err := tx.DeleteBucket([]byte(topic))
-		if err != nil {
-		}
-		fmt.Println("Deleted topic ", topic)
-		return nil
-	})
 }
--- topics.go
+++ topics.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/boltdb/bolt"
+)
+
+// A new topic is mapped to a bucket.
+func addNewTopic(topic string) {
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(topic))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func listTopics() {
+	db.View(func(tx *bolt.Tx) error {
+		return tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+			fmt.Println(markDone, string(name))
+			return nil
+		})
+	})
+}
+
+func deleteTopic(topic string) {
+	db.Update(func(tx *bolt.Tx) error {
+		err := tx.DeleteBucket([]byte(topic))
+		if err != nil {
+		}
+		fmt.Println("Deleted topic ", topic)
+		return nil
+	})
+}
